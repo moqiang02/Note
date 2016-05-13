4f52457df1c650d86da899f84647bed5d5bf4b6d
Rex Mon <Rex Mon>
2016-05-13 08:18:46 +0800

    rex

--- app/src/main/java/com/example/rex/note/App.java
+++ app/src/main/java/com/example/rex/note/App.java
@@ -32,7 +32,7 @@ public class App extends Application {
     }
 
     private void setupDatabase() {
-        // 通过 DaoMaster 的内部类 DevOpenHelper，你可以得到一个便利的 SQLiteOpenHelper 对象。
+        // 通过DaoMaster的内部类 DevOpenHelper，你可以得到一个便利的 SQLiteOpenHelper 对象。
         // 可能你已经注意到了，你并不需要去编写「CREATE TABLE」这样的 SQL 语句，因为 greenDAO 已经帮你做了。
         // 注意：默认的 DaoMaster.DevOpenHelper 会在数据库升级时，删除所有的表，意味着这将导致数据的丢失。
         // 所以，在正式的项目中，你还应该做一层封装，来实现数据库的安全升级。
